cfa5a8a88cbd56c36c2a22af0ac226aba5dcee1d
interpreted-keyword-9673 <mel.cantorias@gmail.com>
2020-02-17 12:23:13 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,3 @@
 # Define the variable board below.
 
-board_top = [" ", " ", " "]
-board_middle = [" ", " ", " "]
-board_bottom = [" ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
